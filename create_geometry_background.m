d0c0575542795e6b9841880e5650fe1bf525cc8f
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-07-18 20:13:36 +0300

    Delete create_geometry_background.m

--- create_geometry_background.m
+++
@@ -1,8 +0,0 @@
-%Copyright © 2019- Sampsa Pursiainen & GPU-ToRRe Development Team
-%See: https://github.com/sampsapursiainen/GPU-Torre
-
-pderect(0.4*[-1 1 -1 1],'SQ1'); 
-pdecirc(0,0,0.16,'C1');
-boundary_param = 64;
-make_boundary_curve;
-pdepoly(p(:,1),p(:,2),'P1');
