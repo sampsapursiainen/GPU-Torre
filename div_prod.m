262a1a6aa9be3a1673b3a7da8a32dffb2a48099d
Sampsa Pursiainen <sampsa.pursiainen@iki.fi>
2021-07-18 20:16:23 +0300

    Delete div_prod.m

--- div_prod.m
+++
@@ -1,9 +0,0 @@
-%Copyright © 2019- Sampsa Pursiainen & GPU-ToRRe Development Team
-%See: https://github.com/sampsapursiainen/GPU-Torre
-
-function [div_vec] = div_prod(B_1,B_2,p_1,p_2,triangles)
-
-aux_vec = B_1.*p_1 + B_2.*p_2;
-div_vec = double(accumarray(triangles(:),aux_vec(:)));
-
-end
